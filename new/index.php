54eec4087fa84a3eb3fc2dbf1fac05ddcb45f986
aeril02 <axel.moerkerke@gmail.com>
2024-10-23 15:16:08 +0200

    initalisation index.php

--- new/index.php
+++ new/index.php
@@ -1,2 +1,18 @@
+<?php 
+    include '.../config/db_config.php';
+?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href=".../source/css/style.css">
+    <script src=".../source/js/script.js" defer></script>
+</head>
+<body>
+    <?php include '../source/PHP/header.php'; ?>
+</body>
 </html>

aea7af29eb057ce9af8bed0dd2c3df2e454f1fe8
aeril02 <axel.moerkerke@gmail.com>
2024-10-24 09:26:43 +0200

    creation foot/head + squelette index

--- page/index.php
+++ page/index.php
@@ -15,8 +15,30 @@
 <body>
     <header>
         <nav>
-            <ul id="menu"></ul>
+            <ul id="header"></ul>
         </nav>
     </header>
+        
+        
+    <main>
+        <h1 id="tittleIndex">Presentation du ZOO </h1>
+        <div id ="presentationZooIndex">   TXT TXT TXT </div>
+  
+    </main>
+
+    <section>
+        <h2 id = "commentZooIndex">laisser un avis sur notre ZOO </h2>
+        <div id="commentZooIndex">  TXT TXT TXT </div>
+        <nav>
+            <ul id ="commentairesIndex"></ul>
+        </nav>
+    </section>
+    
+    <footer>
+        <na>
+            <ul id="footer"></ul>
+        </na>
+    </footer>
+
 </body>
 </html>
--- source/java/header.js
+++ source/java/header.js
@@ -1,17 +1,17 @@
 // header pour les page du site 
 // creation d'un array pour la creation du menu
 const menuItems = [
+    {src : "photo/logo_arcadia.webp", alt: "logo arcadia"},
     {href: "../../page/index.php", label: "Accueil"},
     {href: "../../page/service.php", label: "Services"},
     {href: "../../page/habitats.php", label: "Habitats"},
     {href: "../../page/contact.php", label: "Contact"},
-    {href: "../../page/login.php", label: "Connexion"},
 ]
 // recuperer le chemin actuel
 const currentPath = window.location.pathname;
 
 // creation de la boucle for pour l'affichage du menu 
-const menu = document.getElementById("menu");
+const menu = document.getElementById("header");
 menuItems.forEach(item => {
     const li = document.createElement("li");
     const a = document.createElement("a");
@@ -25,4 +25,23 @@ menuItems.forEach(item => {
 
     li.appendChild(a);
     menu.appendChild(li);
+});
+
+//footer pour les pages du site 
+// creation d'un array pour la creation du menu
+const footerItems = [
+    {href: "../../page/legal.html", label: "Mentions legales"},
+    {href: "../../page/billeterie.php", label: "Billetterie"},
+    {href: "../../page/reglement.php", label: "Réglement interne"},
+    {href: "../../page/recrutement.html", label: "Recrutement"},
+    {href: "../../page/login.php", label: "Connexion"},
+]
+const footer = document.getElementById("footer");
+footerItems.forEach(item => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = item.href;
+    a.textContent = item.label;
+    li.appendChild(a);
+    footer.appendChild(li);
 });

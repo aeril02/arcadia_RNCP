a3d9f0a1fad5cd71288a84add35dbb9c7b0d64c1
aeril02 <axel.moerkerke@gmail.com>
2024-10-29 18:45:33 +0100

    debu de creation du bouton pour section.php

--- source/php/forHabitat/Section.php
+++ source/php/forHabitat/Section.php
@@ -10,9 +10,10 @@ try {
     // Récupérer les informations des habitats et des animaux associés
     $stmt = $conn->prepare("
         SELECT habitat.habitat_id, habitat.nom AS habitat_nom, habitat.description, habitat.commentaire_habitat,habitat.photo AS photo_habitat,
-               animal.animal_id, animal.prenom, animal.etat, animal.image
+               animal.animal_id, animal.prenom, animal.etat, animal.image, animal.race_id, race.label AS race_label
         FROM habitat
         LEFT JOIN animal ON habitat.habitat_id = animal.habitat_id
+        LEFT JOIN race ON animal.race_id = race.race_id
         ORDER BY habitat.habitat_id, animal.animal_id
     ");
     $stmt->execute();
@@ -38,7 +39,8 @@ try {
             $habitats[$habitat_id]['animaux'][] = [
                 'prenom' => $row['prenom'],
                 'etat' => $row['etat'],
-                'image' => base64_encode($row['image']) // Convertir l'image en base64 pour affichage
+                'race' => $row['race_label'],
+                'image' => base64_encode($row['image']) // Convertir l'image en base64 pour affichage  
             ];
         }
     }
@@ -61,13 +63,14 @@ $conn = null;
                 </figure>
                 <aside class="boutonAnimaux">
                     <?php foreach ($habitat['animaux'] as $animal): ?>
-                        <bouton class="animal">
-                            <p><strong><?php echo htmlspecialchars($animal['prenom']); ?></strong></p>
-                            <p>État: <?php echo htmlspecialchars($animal['etat']); ?></p>
-                            <?php if ($animal['image']): ?>
-                                <img src="data:image/webp;base64,<?php echo $animal['image']; ?>" alt="Image de <?php echo htmlspecialchars($animal['prenom']); ?>">
-                            <?php endif; ?>
-                        </bouton>
+                            <details  class="boutonAnimal">
+                                <summary><strong><?php echo htmlspecialchars($animal['race']); ?></strong></summary>
+                                <p><strong><?php echo htmlspecialchars($animal['prenom']); ?></strong></p>
+                                <p>État: <?php echo htmlspecialchars($animal['etat']); ?></p>
+                                <?php if ($animal['image']): ?>
+                                    <img src="data:image/webp;base64,<?php echo $animal['image']; ?>" alt="Image de <?php echo htmlspecialchars($animal['prenom']); ?>">
+                                <?php endif; ?> // fin de la condition if
+                            </details>
                     <?php endforeach; ?>
                 </aside>
             </aside>
